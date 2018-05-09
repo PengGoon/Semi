2913af70b3f864f55e1f63550d4b545b621327ce
Ok.Ji-Hoon <ozh9508@naver.com>
2018-05-09 15:02:38 +0900

    상품 dao,dto 추가

--- src/com/mvc/dao/ProductDAO.java
+++ src/com/mvc/dao/ProductDAO.java
@@ -1,9 +1,16 @@
 package com.mvc.dao;
 
+import com.mvc.dto.ProductDTO;
+
 public class ProductDAO {
 
 	public ProductDAO() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public int write(ProductDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
--- src/com/mvc/dto/ProductDTO.java
+++ src/com/mvc/dto/ProductDTO.java
@@ -1,9 +1,92 @@
 package com.mvc.dto;
 
+import java.sql.Date;
+
 public class ProductDTO {
-
-	public ProductDTO() {
-		// TODO Auto-generated constructor stub
+	
+	private int prd_Id;
+	private String sell_Id;
+	private int cateFirst_Id;
+	private int cateSecond_Id;
+	private String prd_Name;
+	private int prd_Price;
+	private int prd_Count;
+	private String prd_From;
+	private int prd_Soldout;
+	private Date prd_Date;
+	private int prd_bHit;
+	private String newFileName;
+	
+	public int getPrd_Id() {
+		return prd_Id;
 	}
-
+	public void setPrd_Id(int prd_Id) {
+		this.prd_Id = prd_Id;
+	}
+	public String getSell_Id() {
+		return sell_Id;
+	}
+	public void setSell_Id(String sell_Id) {
+		this.sell_Id = sell_Id;
+	}
+	public int getCateFirst_Id() {
+		return cateFirst_Id;
+	}
+	public void setCateFirst_Id(int cateFirst_Id) {
+		this.cateFirst_Id = cateFirst_Id;
+	}
+	public int getCateSecond_Id() {
+		return cateSecond_Id;
+	}
+	public void setCateSecond_Id(int cateSecond_Id) {
+		this.cateSecond_Id = cateSecond_Id;
+	}
+	public String getPrd_Name() {
+		return prd_Name;
+	}
+	public void setPrd_Name(String prd_Name) {
+		this.prd_Name = prd_Name;
+	}
+	public int getPrd_Price() {
+		return prd_Price;
+	}
+	public void setPrd_Price(int prd_Price) {
+		this.prd_Price = prd_Price;
+	}
+	public int getPrd_Count() {
+		return prd_Count;
+	}
+	public void setPrd_Count(int prd_Count) {
+		this.prd_Count = prd_Count;
+	}
+	public String getPrd_From() {
+		return prd_From;
+	}
+	public void setPrd_From(String prd_From) {
+		this.prd_From = prd_From;
+	}
+	public int getPrd_Soldout() {
+		return prd_Soldout;
+	}
+	public void setPrd_Soldout(int prd_Soldout) {
+		this.prd_Soldout = prd_Soldout;
+	}
+	public Date getPrd_Date() {
+		return prd_Date;
+	}
+	public void setPrd_Date(Date prd_Date) {
+		this.prd_Date = prd_Date;
+	}
+	public int getPrd_bHit() {
+		return prd_bHit;
+	}
+	public void setPrd_bHit(int prd_bHit) {
+		this.prd_bHit = prd_bHit;
+	}
+	public String getNewFileName() {
+		return newFileName;
+	}
+	public void setNewFileName(String newFileName) {
+		this.newFileName = newFileName;
+	}
 }
