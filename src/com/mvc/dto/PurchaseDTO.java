6eccf1a713ac9629cc586657aedef96a5a7e939f
KimHW <jp10930@naver.com>
2018-05-17 14:31:54 +0900

    구매내역 서비스, DAO, DTO 수정

--- src/com/mvc/dto/PurchaseDTO.java
+++ src/com/mvc/dto/PurchaseDTO.java
@@ -1,5 +1,62 @@
 package com.mvc.dto;
 
-public class PurchaseDTO {
+import java.sql.Date;
 
+public class PurchaseDTO {
+	private int pur_id;
+	private String user_id;
+	private int prd_id;
+	private String sell_Id;
+	private int pur_count;
+	private Date pur_date;
+	private String pur_state;
+	private int pur_delievery;
+	public int getPur_id() {
+		return pur_id;
+	}
+	public void setPur_id(int pur_id) {
+		this.pur_id = pur_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getPrd_id() {
+		return prd_id;
+	}
+	public void setPrd_id(int prd_id) {
+		this.prd_id = prd_id;
+	}
+	public String getSell_Id() {
+		return sell_Id;
+	}
+	public void setSell_Id(String sell_Id) {
+		this.sell_Id = sell_Id;
+	}
+	public int getPur_count() {
+		return pur_count;
+	}
+	public void setPur_count(int pur_count) {
+		this.pur_count = pur_count;
+	}
+	public Date getPur_date() {
+		return pur_date;
+	}
+	public void setPur_date(Date pur_date) {
+		this.pur_date = pur_date;
+	}
+	public String getPur_state() {
+		return pur_state;
+	}
+	public void setPur_state(String pur_state) {
+		this.pur_state = pur_state;
+	}
+	public int getPur_delievery() {
+		return pur_delievery;
+	}
+	public void setPur_delievery(int pur_delievery) {
+		this.pur_delievery = pur_delievery;
+	}
 }
--- src/com/mvc/service/PurchaseService.java
+++ src/com/mvc/service/PurchaseService.java
@@ -1,12 +1,32 @@
 package com.mvc.service;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.mvc.dao.ProductDAO;
+import com.mvc.dto.ProductDTO;
+
 public class PurchaseService {
 
-	public void purch(HttpServletRequest request, HttpServletResponse response) {
-		
+	public void purch(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//request 전달
+		ProductUpload upload = new ProductUpload(request);
+		//regist 메서드를 사용해서 사진 등록
+		//상품명,판매자,판매가,원산지,물품수량,파일명을 받는다.ProductDTO
+		ProductDTO dto = upload.regist();
+		//DAO 에 dto 를 전달하여 DB 에 추가해 달라고 요청
+		ProductDAO dao = new ProductDAO();
+		int pk = dao.write(dto);
+		//실패(글쓰기 폼)
+		String page = "prd_writeForm.jsp";
+		//결과에 따라 페이지 이동
+		if(pk > 0) {
+			//성공(상세보기) = 글쓰기 한 후 idx 반환
+			//page = "detail?id="+pk;
+			page = "index.jsp";
+		}
+		response.sendRedirect(page);
 	}
-
 }
